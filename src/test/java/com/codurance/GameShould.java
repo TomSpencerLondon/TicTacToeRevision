94758f44e5fd06866edb329942ec1d5c0493ab6d
Tom Spencer <tom@homeflow.co.uk>
2020-08-21 11:31:03 +0100

    Add static imports

--- src/test/java/com/codurance/GameShould.java
+++ src/test/java/com/codurance/GameShould.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 
 import static com.codurance.Player.O;
 import static com.codurance.Player.X;
+import static com.codurance.Square.TOP_LEFT;
+import static com.codurance.Square.TOP_MIDDLE;
 import static com.codurance.Status.SQUARE_ALREADY_PLAYED;
 import static com.codurance.Status.START;
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -22,21 +24,21 @@ public class GameShould {
   @Test
   void wait_for_O_to_play_after_x() {
     Game game = new Game();
-    game = game.play(Square.TOP_LEFT);
+    game = game.play(TOP_LEFT);
     assertThat(game.state()).isEqualTo(new GameState(START, O));
   }
 
   @Test
   void alternate_the_players() {
     Game game = new Game();
-    game = game.play(Square.TOP_LEFT);
-    game = game.play(Square.TOP_MIDDLE);
+    game = game.play(TOP_LEFT);
+    game = game.play(TOP_MIDDLE);
     assertThat(game.state()).isEqualTo(new GameState(START, X));
   }
 
   @Test
   void not_permit_square_to_be_played_twice() {
-    Game game = play(Square.TOP_LEFT, Square.TOP_MIDDLE, Square.TOP_LEFT);
+    Game game = play(TOP_LEFT, TOP_MIDDLE, TOP_LEFT);
     assertThat(game.state()).isEqualTo(new GameState(SQUARE_ALREADY_PLAYED, X));
   }
 
